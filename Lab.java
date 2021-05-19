548a3b886d1f23ebd4a1ffbe683ea87266e8fdd4
carmenmk <35529232+carmenmk@users.noreply.github.com>
2021-05-19 18:42:55 +0100

    .saveFile() updated
    Updated to save the two arraylists that are modified throughout the program

--- Lab.java
+++ Lab.java
@@ -3,14 +3,9 @@ import java.io.*;
 import java.util.*;
 
 public class Lab {
-
-	// Originally called 'File' 
-	
-	
-    //Instance Variables
     private  ArrayList<String> labList = new ArrayList<String>();
-    private String userDir = System.getProperty("user.home");
-    //
+    // static - an issue 
+    private static String userDir = System.getProperty("user.home");
 
     //Getter Method for Arraylist
     public ArrayList<String> getLabList() {
@@ -18,10 +13,11 @@ public class Lab {
     }
 
     //Getter Method for String name.
-    public String getUserDir() {
+    public static String getUserDir() {
         return userDir;
     }
-
+    
+    
     //Method to upload a document and converts the text into an ArrayList
     public  ArrayList<String> uploadFile(){
         File file;
@@ -61,16 +57,16 @@ public class Lab {
             //System.out.println(getLabList());
         }
 		return getLabList();
-
     }
 
+    // Not used: 
     public void clearList(){
         //Clears the Arraylist
         getLabList().clear();
     }
 
+    // Not used: 
     public void addRequirement(){
-
         System.out.println("Add new requirement: ");
         //Create New Scanner Object
         Scanner newCatch = new Scanner(System.in);
@@ -80,57 +76,86 @@ public class Lab {
         System.out.println(getLabList());
     }
 
-    public void createFile(){
-
-        //Initialize File Object and Passing Path as Argument
-        File file = new File(getUserDir() + "/Desktop/LabList.txt");
-        boolean result;
-        try
-        {
-            result = file.createNewFile();      //Creates a New File
-            if(result)      // Test if Successfully Created a New File
-            {
-                System.out.println("file created "+file.getCanonicalPath()); //Returns the Path String
-            }
-            else
-            {
-                System.out.println("File already exist at location: "+file.getCanonicalPath());
-            }
-        }
-        catch (IOException e)
-        {
-            e.printStackTrace();    //Prints Exception if any
-        }
-    }
-
-    public void writeFile(){ 
-
-        //First Create File, Before Writing it
-        createFile();
-
-        String fullList = "";
-
-        FileWriter writer;
-        try {
-            writer = new FileWriter(getUserDir() + "/Desktop/LabList.txt");
-            //Go through entire ArrayList and Concatenating Strings into String FullList
-            for(String section : getLabList()){
-                fullList = fullList + section + "\n";
-            }
-            //Write String FullList into the File
-            writer.write(fullList);
-            writer.close();
-        } catch (IOException e) {
-            e.printStackTrace();        //Prints Exception if any
-        }
-    }
     
-//    // ****
-//    public void setFlag(int flag) {
-//    	this.flag = flag; 
+//    
+//    public void createFile(){
+//        //Initialize File Object and Passing Path as Argument
+//        File file = new File(getUserDir() + "/Desktop/LabList.txt");
+//        boolean result;
+//        try
+//        {
+//            result = file.createNewFile();      //Creates a New File
+//            if(result)      // Test if Successfully Created a New File
+//            {
+//                System.out.println("file created "+file.getCanonicalPath()); //Returns the Path String
+//            }
+//            else
+//            {
+//                System.out.println("File already exist at location: "+file.getCanonicalPath());
+//            }
+//        }
+//        catch (IOException e)
+//        {
+//            e.printStackTrace();    //Prints Exception if any
+//        }
+//    }
+
+    
+    
+    public static void saveFile(ArrayList<Course> courses, ArrayList<Teacher> teachers) {
+	    BufferedWriter out = null; 
+    	StringBuffer sb = new StringBuffer();
+    	StringBuffer sb2 = new StringBuffer();
+    	String userHomeFolder = getUserDir(); 
+    	File updatedData = new File(userHomeFolder, "LabList.txt");
+	    try {
+	    	// Saves to home directory 
+	    	out = new BufferedWriter(new FileWriter(updatedData));
+	    	// Courses 
+	            for (Course s : courses) {
+	              sb.append(s + "\n");
+	            }
+	        String coursesInString = sb.toString();
+	         out.write(coursesInString);
+	        // Teachers
+		  for (Teacher s2 : teachers) {
+		       sb2.append(s2 + "\n");
+		     }
+	       String teachersInString = sb2.toString();
+	       out.write(teachersInString);
+	    } catch (IOException e) {
+	    	e.printStackTrace();
+	    } finally { 
+	    	try {
+			out.close(); 
+		} catch (IOException e) {
+			e.printStackTrace(); 
+		}
+	    }
+	}
+    
+//    
+//  
+//    public void writeFile(){ 
+//        //First Create File, Before Writing it
+////        createFile();
+//
+//        String fullList = "";
+//
+//        FileWriter writer;
+//        try {
+//            writer = new FileWriter(getUserDir() + "/Desktop/LabList.txt");
+//            //Go through entire ArrayList and Concatenating Strings into String FullList
+//            for(String section : getLabList()){
+//                fullList = fullList + section + "\n";
+//            }
+//            //Write String FullList into the File
+//            writer.write(fullList);
+//            writer.close();
+//        } catch (IOException e) {
+//            e.printStackTrace();        //Prints Exception if any
+//        }
 //    }
 //    
-//    public int getFlag() {
-//    	return this.flag; 
-//    }
+
 }
