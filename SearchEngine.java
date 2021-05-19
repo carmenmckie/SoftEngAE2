fad5908e6c0482378a43d29f1e4abdf5582c1618
carmenmk <35529232+carmenmk@users.noreply.github.com>
2021-05-19 10:25:47 +0100

    Rough method to save to file
    Saves to file when button on GUI is pressed, before closing the program

--- SearchEngine.java
+++ SearchEngine.java
@@ -1,31 +1,27 @@
 import java.util.ArrayList;
 import java.util.Scanner;
-//Search engine is a Singleton since we only ever need one of these objects 
+import java.io.FileWriter;
+import java.io.IOException;
+//TODO make search engine into Singleton to make this satisfy the requirements becasue we only ever need one of these objects 
 
 public class SearchEngine {
 	private ArrayList <Teacher> teacherList;
 	private ArrayList <Course> courseList; 
 	
-//	public SearchEngine(ArrayList<Teacher> teacherList, ArrayList<Course> courseList) { //, ArrayList <String> teacherSkills) {
-//		this.teacherList = teacherList;
-//		this.courseList = courseList; 
-//	}
-	
+
 	private static SearchEngine se = new SearchEngine();
 	
-	private SearchEngine() {
-		
-	}
-	
+
 	public static SearchEngine getSearchEngine() {
 		return se;
 	}
-	
 	
 	public void setAll(ArrayList<Teacher> teacherList, ArrayList<Course> courseList) {
 		this.teacherList = teacherList;
 		this.courseList = courseList; 
 	}
+	
+	
 	
 	//method to find a teacher by passing it a skill
 	public ArrayList<Teacher> findTeacher(String skill) {
@@ -46,9 +42,7 @@ public class SearchEngine {
 					tempTeacher = this.getTeacherList().get(i);					
 			}
 		} return tempTeacher; 
-	}
-	
-	
+	}	        
 	
 	// Testing: 
 	// To add skill to specific teacher 
@@ -70,36 +64,7 @@ public class SearchEngine {
 		return result; 
 		}
 	
-	// *****************************
-	// Add Teacher to Lab 
-	// The order the user should enter is 1. Course 2. Lab 3. Teacher Name 
-//	public Course addTeacherToLab(String data) {
-//		Course tempCourse = new Course(); 
-//		Scanner s = new Scanner(data); 
-//		String course = s.next(); 
-//		String lab = s.next(); 
-//		String teacher = s.next(); 
-//		// Look for the Course: 
-//		for(int i=0; i < this.getCourseList().size(); i++) {
-//			// Find the Course the user entered: 
-//			if(this.getCourseList().get(i).getCourseName().contains(course)) {
-//				tempCourse = this.getCourseList().get(i);
-//				// Find the Lab within that course: 
-//				for(int j = 0; j < this.getCourseList().get(i).getLabList().size(); j++) {
-//					if(this.getCourseList().get(i).getLabList().get(j).getName().contains(lab)) {
-//						// Add the Teacher requested to that specific Lab 
-//						this.getCourseList().get(i).getLabList().get(j).getTeachersInLab().add(findTeacherByName(teacher));
-//						// find teacher 
-//					}
-//				}
-//				
-//				
-//			}
-//		}	return tempCourse; 				
-//	}
-//		
-//	
-	
+
 	public void addTeacherToLab(String data) {
 //		Course tempCourse = new Course(); 
 		Scanner s = new Scanner(data); 
@@ -124,7 +89,37 @@ public class SearchEngine {
 			}
 		}					
 	}
-		
+	
+	// *********************************************************************************
+	// Actual directory needs to be updated to be dynamic 
+	public static void saveFile(ArrayList<Course> courses, ArrayList<Teacher> teachers) {
+	    FileWriter fw = null; 
+    	StringBuffer sb = new StringBuffer();
+    	StringBuffer sb2 = new StringBuffer();
+	    try {
+	    	fw = new FileWriter("/Users/cmckie/Desktop/Requirements.txt");
+	    	// Courses 
+	            for (Course s : courses) {
+	              sb.append(s + "\n");
+	            }
+	        String coursesInString = sb.toString();
+	         fw.write(coursesInString);
+	        // Teachers
+		  for (Teacher s2 : teachers) {
+		       sb2.append(s2 + "\n");
+		     }
+	       String teachersInString = sb2.toString();
+	       fw.write(teachersInString);
+	    } catch (IOException e) {
+	    	e.printStackTrace();
+	    } finally { 
+	    	try {
+			fw.close(); 
+		} catch (IOException e) {
+			e.printStackTrace(); 
+		}
+	    }
+	}
 	
 	
 	
