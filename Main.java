e1bd0dce9bc5c9951dee7a9b823a0679ee8f7404
carmenmk <35529232+carmenmk@users.noreply.github.com>
2021-05-19 16:56:28 +0100

    Main changed as part of fixing double upload issue

--- Main.java
+++ Main.java
@@ -1,28 +1,11 @@
-import java.util.ArrayList;
-import java.util.Scanner;
 
 public class Main {
 
-	// Opening two FileReaders, may be fixed by making this into a seperate method / class 
-	// Not had a chance to make this into seperate methods yet 
-	
 	public static void main(String[] args) {
-
-		//!!!!!!! Because of two .uploadFiles() it opens a JFileChooser twice, needs fixed.... 
-		Lab lab1 = new Lab();
-		ArrayList<String> courseInfo = new ArrayList<>();
-		courseInfo = lab1.uploadFile();
-		
-		ArrayList<Course> courses = new ArrayList<>();
-		ArrayList<Teacher> teachers = new ArrayList<>();
-		DataHandling dh = new DataHandling(courses, teachers, courseInfo);
-		dh.dataHandler();
-		//
-		View GUI = new View(lab1); 
-		SearchEngine searchEngine = SearchEngine.getSearchEngine(); 
-		searchEngine.setAll(teachers, courses);
-		GUI.setSearchEngine(searchEngine);
-		
-	//	lab1.writeFile();
-	}
-}
+				Lab lab1 = new Lab();
+				View GUI = new View(lab1); 
+				SearchEngine searchEngine = SearchEngine.getSearchEngine(); 
+				searchEngine.setAll(GUI.getTeacherList(), GUI.getCourseList());
+				GUI.setSearchEngine(searchEngine);
+			}
+		}
