46e5fd7e30efb09e84a3a28984f53ec099c06a36
carmenmk <35529232+carmenmk@users.noreply.github.com>
2021-05-21 23:04:40 +0100

    Updating Teacher
    - Deleted .userEnters() (was not used elsewhere)
    - Deleted commented-out code

--- Teacher.java
+++ Teacher.java
@@ -5,8 +5,6 @@ import java.util.Scanner;
 public class Teacher {
 	private String teacherName;
 	private ArrayList <String> teacherSkills = new ArrayList<String>();
-//	private ArrayList <String> teacherTraining = new ArrayList<String>();
-
 	
 	//constructor 
 	public Teacher(String teacherName, ArrayList<String> teacherSkills) {
@@ -19,55 +17,8 @@ public class Teacher {
 		this.teacherSkills = new ArrayList<String>();
 	}
 
-	//User enters teacherSkills or teacher Training
-	public ArrayList<String> userEnters(){
-		String contEnter;
-	    System.out.println("Please enter");
-	    ArrayList<String> contents= new ArrayList<String>();
-        Scanner contentEnter = new Scanner(System.in);
-        contEnter = contentEnter.next();
-        //courseInst.add(courseInstructors); 
-
-        for(String contEnter1 : contents) {
-        	contEnter1 = contEnter;
-            contents.add(contEnter1);
-        }
-        
         return contents;
 	}
-	
-	//Update teacher training information
-//	public ArrayList<String> updateTrainingInfo(ArrayList<String> teacherSkills){
-//		
-//		//User input
-//		System.out.println("Please enter the teacher skills of the training status you want to change, followed by the return key.");
-//		Scanner info = new Scanner(System.in);
-//		String changInfo = info.nextLine();	
-//		
-//		
-//		//Find the corresponding location
-//		int location = 0;
-//		for(String str:teacherSkills){
-//		     if(str.contains(changInfo)){
-//		    	 location = teacherSkills.indexOf(str);
-//		     }
-//		}
-//		
-//		
-//		//Change the data in teacherTraining
-//		teacherTraining.set(location, "OK");  //Trained status is "OK"	
-//		return teacherTraining;
-//	}
-	
-	//Increase teacher's skills
-//	public void addSkill(ArrayList<String> teacherSkills) {
-//		System.out.println("Please enter a new skill, followed by the return key.");
-//		Scanner info = new Scanner(System.in);
-//		String newSkill = info.nextLine();	
-//		
-//		teacherSkills.add(newSkill);
-//		
-//	}
 	
 	public void setSkill(String skill) {
 		this.teacherSkills.add(skill);
@@ -75,23 +26,11 @@ public class Teacher {
 	
 	
 	//setting and getting
-//	public String getTeacherName() {
-//		return teacherName;
-//	}
+
 	public void setTeacherName(String teacherName) {
 		this.teacherName = teacherName;
 	}
 	
-	
-//	public void setTeacherSkills(ArrayList<String> teacherSkills) {
-//		this.teacherSkills = teacherSkills;
-//	}
-//	public ArrayList<String> getTeacherTraining() {
-//		return teacherTraining;
-//	}
-//	public void setTeacherTraining(ArrayList<String> teacherTraining) {
-//		this.teacherTraining = teacherTraining;
-//	}
 
 	public String printSkills() {
 		String s = "";
